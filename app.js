0351dd53cc0e7f509954d1334b7f9637b4a29f4e
Isaac Carnes <isaacjcarnes@gmail.com>
2023-10-04 16:11:30 -0400

    Added ROM folder to gitignore

--- app.js
+++ app.js
@@ -3,38 +3,52 @@ const fs = require("fs");
 
 const hostname = "localhost";
 const port = 3000;
+const availableGames = ["/Flicky.md", "/PokemonRed.gb", "/BigBangMini.nds"];
 const server = http.createServer((req, res) => {
   let urlName = req.url;
   if (urlName == "/") {
-    
-    /*let core = '"segaMD"';
-    let gameName = '"Flicky"';
-    let gameUrl = '"Flicky.md"';*/
-    let core = '"gb"';
-    let gameName = '"Pokemon - Red"'
-    let gameUrl = '"PokemonRed.gb"'
-    let htmlData = baseHtml.replace('"core"', core);
-    htmlData = htmlData.replace('"gameName"', gameName);
-    htmlData = htmlData.replace('"gameUrl"', gameUrl);
+    let htmlData = loadGame(availableGames[0]);
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(htmlData);
-  } else if (urlName == "/Flicky.md") {
-    readFile("./ROMs/Flicky.md").then((data) => {
+  } else if (availableGames.includes(urlName)) {
+    readFile("./ROMs" + urlName).then((data) => {
       res.writeHead(200, { "Content-Type": "text/plain" });
       res.end(data);
     });
-  } else if (urlName == "/PokemonRed.gb") {
-    readFile("./ROMs/PokemonRed.gb").then((data) => {
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end(data);
-    });
-  }else {
+  } else {
     console.log(req.url);
     res.end();
   }
 });
 
-const readFile = (path, encoding=null) => {
+const loadGame = (urlName) => {
+  let core = "";
+  let gameName = "";
+  let gameUrl = "";
+  switch (urlName) {
+    case availableGames[0]:
+      core = '"segaMD"';
+      gameName = '"Flicky"';
+      gameUrl = '"Flicky.md"';
+      break;
+    case availableGames[1]:
+      core = '"gb"';
+      gameName = '"Pokemon - Red"';
+      gameUrl = '"PokemonRed.gb"';
+      break;
+    case availableGames[2]:
+      core = '"nds"';
+      gameName = '"Big Bang Mini"';
+      gameUrl = '"BigBangMini.nds"';
+      break;
+  }
+  let htmlData = baseHtml.replace('"core"', core);
+  htmlData = htmlData.replace('"gameName"', gameName);
+  htmlData = htmlData.replace('"gameUrl"', gameUrl);
+  return htmlData;
+};
+
+const readFile = (path, encoding = null) => {
   return new Promise((resolve, reject) => {
     fs.readFile(path, encoding, function (error, data) {
       if (error) {
